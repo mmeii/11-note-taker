d20befd527ed649c4ff1c9b7104c248ca31d05b5
Mengmei Tu <redmmeii@gmail.com>
2021-01-07 15:57:04 -0600

    update routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,43 @@
 //api routes
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
-// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+module.exports = function (app) {
+    // The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
 
-// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+        let notes = JSON.parse(data);
 
-// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+        // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+        app.get("/api/notes", function (req, res) {
+            res.json(notes);
+        });
+
+        // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+        app.post("/api/notes", function (req, res) {
+            let newNotes = req.body;
+
+            console.log(newNotes);
+            notes.push(newNotes);
+            updateNotes();
+
+        });
+
+        // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+        app.delete("/api/notes/:id", function (req, res) {
+            notes.splice(req.params.id, 1);
+            updateNotes();
+        });
+
+        //update notes for add or delete notes
+        function updateNotes() {
+            fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+                if (err) throw err;
+                return true;
+            })
+        }
+    });
+}
+
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Dependencies
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 //set up express app
 const app = express();
@@ -10,13 +9,12 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
 
 //routers
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-// The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
 
 //starts server to begin listening
 app.listen(PORT, function () {
