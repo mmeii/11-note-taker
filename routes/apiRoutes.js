bc78145a6b78b1ba2b94cca728b6026e3d2c686f
Mengmei Tu <redmmeii@gmail.com>
2021-01-08 17:54:12 -0600

    update apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,35 @@
 //api routes
 const fs = require('fs');
+const notesData = require('../db/db.json');
 
 module.exports = function (app) {
     // The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
-    let allNotes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 
     // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
     app.get("/api/notes", function (req, res) {
-        res.json(allNotes);
+        res.json(notesData);
     });
 
-    //     // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-    //     app.post("/api/notes", function (req, res) {
-    //         let lastNotesId;
+    // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    // app.post("/api/notes", function (req, res) {
+    //     let lastNotesId;
 
-    //         if (allNotes.length) {
-    //             lastNotesId = Math.max(...lastNotesId(allNotes.map(notes => notes.id)))
-    //         } else {
-    //             lastNotesId = 0;
-    //         }
+    //     if (allNotes.length) {
+    //         lastNotesId = Math.max(...lastNotesId(allNotes.map(notes => notes.id)))
+    //     } else {
+    //         lastNotesId = 0;
+    //     }
 
-    //         console.log(newNotes);
-    //         notes.push(newNotes);
+    //     console.log(newNotes);
+    //     notes.push(newNotes);
 
-    //         let jsonNotes = JSON.stringify(notes);
-    //         fs.writeFile("db/db.json", jsonNotes, function (err) {
-    //             if (err) {
-    //                 return console.log(err);
-    //             } console.log("Notes successfully saved!");
-    //         })
-    //     });
+    //     let jsonNotes = JSON.stringify(notes);
+    //     fs.writeFile("db/db.json", jsonNotes, function (err) {
+    //         if (err) {
+    //             return console.log(err);
+    //         } console.log("Notes successfully saved!");
+    //     })
+    // });
 
     //     // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
     //     app.delete("/api/notes/:id", function (req, res) {
