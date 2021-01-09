793884b8d07970cb20da8d89eec473ed133302e7
Mengmei Tu <redmmeii@gmail.com>
2021-01-08 19:37:05 -0600

    fix api routes for get and post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,18 +8,19 @@ module.exports = function (app) {
 
     // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
     app.get("/api/notes", function (req, res) {
+        console.log("Getting Notes");
         res.json(notesData);
     });
 
-    // // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-    // app.post("/api/notes", function (req, res) {
-    //     let newNotes = req.body;
+    // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    app.post("/api/notes", function (req, res) {
+        let newNotes = req.body;
 
-    //     notesData.push(newNotes);
-    //     updateNotes();
+        notesData.push(newNotes);
+        updateNotes();
 
-    //     res.json(newNotes);
-    // });
+        res.json(newNotes);
+    });
 
     // // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
     // // app.delete("/api/notes/:id", function (req, res) {
@@ -28,10 +29,10 @@ module.exports = function (app) {
 
     // // });
 
-    // function updateNotes() {
-    //     fs.writeFile("db/db.json", JSON.stringify(notesData, '\t'), err => {
-    //         if (err) throw err;
-    //         return true;
-    //     })
-    // }
+    function updateNotes() {
+        fs.writeFile("db/db.json", JSON.stringify(notesData, '\t'), err => {
+            if (err) throw err;
+            return true;
+        })
+    }
 }
