3f801908d8ae1e50172d43e48351f690ffcb138b
Mengmei Tu <redmmeii@gmail.com>
2021-01-09 19:23:21 -0600

    fix unique id and delete route with matching id, clear db file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,7 +12,6 @@ module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
         console.log("Getting Notes");
         res.json(notesData);
-        console.log("get notes");
     });
 
     // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
@@ -20,30 +19,51 @@ module.exports = function (app) {
 
         let newNotes = req.body;
 
-        newNotes.id = notesData.length;
         console.log(newNotes);
 
         notesData.push(newNotes);
-        updateNotes();
-        console.log(notesData);
 
-        res.json(newNotes);
+        console.log(notesData);
+        console.log(notesData.length);
+
+        //add id to each note
+        for (let i = 0; i < notesData.length; i++) {
+            notesData[i].id = i + 1;
+        }
+
+        const stringifyNotes = JSON.stringify(notesData);
+
+        fs.writeFile('db/db.json', stringifyNotes, 'utf8', (err, data) => {
+            if (err) throw err;
+            console.log("New notes successfully saved!");
+
+        });
+
+        res.json(notesData);
     });
 
     // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
     app.delete("/api/notes/:id", function (req, res) {
         // console.log(typeof (notesData));
+        const noteIdToDelete = parseInt(req.params.id);
+        let notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 
-        notesData.splice(req.params.id, 1);
+        for (let i = 0; i < notes.length; i++) {
+            if (noteIdToDelete === parseInt(notes[i].id)) {
+                notes.splice(i, 1);
+            }
+        };
 
-        console.log(notesData);
-        updateNotes();
+        const stringifyNotes = JSON.stringify(notes);
+
+        fs.writeFile('db/db.json', stringifyNotes, 'utf8', (err, data) => {
+            if (err) throw err;
+            console.log("Note successfully deleted!");
+
+        });
+
+        res.json(notes);
     });
 
-    function updateNotes() {
-        fs.writeFile("db/db.json", JSON.stringify(notesData, '\t'), err => {
-            if (err) throw err;
-            return true;
-        })
-    }
+
 }
